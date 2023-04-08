b9764ec42bd6a558a9dc1b48fba284aa03735a1e
Jordi-De-La-Cruz <115385707+Jordi-De-La-Cruz@users.noreply.github.com>
2023-04-07 23:03:41 -0500

    echo "# MyFirstApp" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Jordi-De-La-Cruz/ MyFirstApp.git  git push -u origen principal

---
+++ app/src/main/java/tecsup/edu/pe/SplashActivity.kt
@@ -0,0 +1,24 @@
+package tecsup.edu.pe
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                )
+            )
+        }, 3000)
+
+    }
+}
